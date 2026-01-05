0cd77aaffcd141ec450949c6952861f6dcfa856c
Khyati Uttam <ukhyati05@gmail.com>
2026-01-05 23:34:08 +0530

    Add LeetCode 125 - Valid Palindrome

---
+++ Strings /Easy/125_Valid_Palindrome.java
@@ -0,0 +1,29 @@
+// LeetCode 125: Valid Palindrome
+// Approach: Use two pointers to compare alphanumeric characters from both ends.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
