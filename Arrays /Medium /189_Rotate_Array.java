7b812dcf31d9d71194399b9f58c5b2d0c083c872
Khyati Uttam <ukhyati05@gmail.com>
2026-01-06 23:02:41 +0530

    Add LeetCode 189 - Rotate Array

---
+++ Arrays /Medium /189_Rotate_Array.java
@@ -0,0 +1,25 @@
+// LeetCode 189: Rotate Array
+// Approach: Reverse the entire array, then reverse first k elements and remaining elements.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
