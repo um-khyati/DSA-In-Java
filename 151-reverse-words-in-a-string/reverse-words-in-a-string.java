edaabd2f9e02b491722a911fe09c62a84cd0c626
Khyati Uttam <ukhyati05@gmail.com>
2026-01-09 23:49:30 +0530

    Time: 4 ms (94.27%) | Memory: 44.5 MB (50.62%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,18 +1,36 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder ans = new StringBuilder();
-        int i = s.length() - 1;
-
-        while (i >= 0) {
-            while (i >= 0 && s.charAt(i) == ' ') i--;
-
-            if (i < 0) break;
-
-            int j = i;
-            while (i >= 0 && s.charAt(i) != ' ') i--;
-            if (ans.length() > 0) ans.append(" ");
-            ans.append(s.substring(i + 1, j + 1));
+        StringBuilder ans = new StringBuilder("");
+        int rightPointer = -1;
+        int leftPointer = -1;
+        
+        // Iterate backwards through the string
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                // If we encounter a space and we were tracking a word
+                if (rightPointer != -1 && leftPointer != -1) {
+                    // Add space before the word if it's not the first word added
+                    if (ans.length() > 0) ans.append(" ");
+                    ans.append(s.substring(leftPointer, rightPointer + 1));
+                    
+                    // Reset pointers
+                    leftPointer = -1;
+                    rightPointer = -1;
+                }
+                continue;
+            }
+            
+            // If it's a character, update pointers
+            if (rightPointer == -1) rightPointer = i; // Mark end of word
+            if (rightPointer != -1) leftPointer = i;  // Mark start of word (keeps moving left)
         }
+        
+        // Append the last word (which corresponds to the first word in original string)
+        if (rightPointer != -1 && leftPointer != -1) {
+            if (ans.length() > 0) ans.append(" ");
+            ans.append(s.substring(leftPointer, rightPointer + 1));
+        }
+        
         return ans.toString();
     }
 }
