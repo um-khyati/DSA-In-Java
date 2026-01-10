0045288c48aa05d2ab69f8713bbf0a180eb398b9
Khyati Uttam <ukhyati05@gmail.com>
2026-01-10 21:03:44 +0530

    Time: 19 ms (75.26%) | Memory: 43.4 MB (91.65%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,27 +1,29 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int n1 = s1.length(), n2 = s2.length();
-        int[][] dp = new int[n1 + 1][n2 + 1];
+        int[] dp = new int[n2 + 1];
 
-        for (int i = 1; i <= n1; i++) {
-            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
-        }
         for (int j = 1; j <= n2; j++) {
-            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
+            dp[j] = dp[j - 1] + s2.charAt(j - 1);
         }
 
         for (int i = 1; i <= n1; i++) {
+            int prev = dp[0];
+            dp[0] += s1.charAt(i - 1);
+
             for (int j = 1; j <= n2; j++) {
+                int temp = dp[j];
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[j] = prev;
                 } else {
-                    dp[i][j] = Math.min(
-                        dp[i - 1][j] + s1.charAt(i - 1),
-                        dp[i][j - 1] + s2.charAt(j - 1)
+                    dp[j] = Math.min(
+                        dp[j] + s1.charAt(i - 1),
+                        dp[j - 1] + s2.charAt(j - 1)
                     );
                 }
+                prev = temp;
             }
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 }
