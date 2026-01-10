aadd553a62fa2d305430675e41269c524a556c45
Khyati Uttam <ukhyati05@gmail.com>
2026-01-10 21:03:13 +0530

    Time: 21 ms (57.82%) | Memory: 46.9 MB (11.18%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,21 +1,27 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int total_sum = 0, n1 = s1.length(), n2 = s2.length();
-        for (int i = 0; i < n1; i++) total_sum += s1.charAt(i);
-        for (int i = 0; i < n2; i++) total_sum += s2.charAt(i);
-        
+        int n1 = s1.length(), n2 = s2.length();
         int[][] dp = new int[n1 + 1][n2 + 1];
+
+        for (int i = 1; i <= n1; i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
+        }
+        for (int j = 1; j <= n2; j++) {
+            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
+        }
+
         for (int i = 1; i <= n1; i++) {
             for (int j = 1; j <= n2; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    int ascii_value = (int)s1.charAt(i - 1);
-                    dp[i][j] = dp[i - 1][j - 1] + ascii_value;
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(
+                        dp[i - 1][j] + s1.charAt(i - 1),
+                        dp[i][j - 1] + s2.charAt(j - 1)
+                    );
                 }
             }
         }
-
-        return total_sum - 2 * dp[n1][n2];
+        return dp[n1][n2];
     }
 }
