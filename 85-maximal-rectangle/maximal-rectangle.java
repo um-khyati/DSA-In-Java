ae9fe10053be493c42aee024a29f719ac7d975f7
Khyati Uttam <ukhyati05@gmail.com>
2026-01-11 21:19:03 +0530

    Time: 15 ms (59.33%) | Memory: 49.1 MB (72.72%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,36 +1,35 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int m = matrix.length, n = matrix[0].length, ans = 0;
-        int[]hist = new int[n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]!='0')hist[j]+=1;
-                else hist[j] = 0;
-            }
-            int area = area(hist);
-            ans = Math.max(ans, area);
-        }
-
-        return ans;
-       
-    }
-
-    public static int area(int[] heights) {
-        int n = heights.length;
-        int maxArea = 0;
+    private int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        int[] arr = new int[n + 1];
+        System.arraycopy(heights, 0, arr, 0, n);
 
         for (int i = 0; i <= n; i++) {
-            int h = (i == n) ? 0 : heights[i];
-            while (!stack.isEmpty() && h < heights[stack.peek()]) {
-                int height = heights[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, height * width);
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                int h = arr[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
             }
             stack.push(i);
         }
-
         return maxArea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int ans = 0;
+
+        for (char[] row : matrix) {
+            for (int j = 0; j < cols; j++) {
+                heights[j] = row[j] == '1' ? heights[j] + 1 : 0;
+            }
+            ans = Math.max(ans, largestRectangleArea(heights));
+        }
+        return ans;
     }
 }
