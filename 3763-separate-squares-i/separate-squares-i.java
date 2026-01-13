7e8ebda71b4de49880ca40e964b20ff85921a042
Khyati Uttam <ukhyati05@gmail.com>
2026-01-13 19:35:29 +0530

    Time: 151 ms (33.68%) | Memory: 163.2 MB (8.95%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,36 +1,38 @@
 class Solution {
     public double separateSquares(int[][] squares) {
+        List<long[]> events = new ArrayList<>();
         long total = 0;
-        HashMap<Long, Long> diff = new HashMap<>();
 
         for (int[] sq : squares) {
             long y = sq[1];
             long s = sq[2];
-            total += s * s;
-            diff.put(y, diff.getOrDefault(y, 0L) + s);
-            diff.put(y + s, diff.getOrDefault(y + s, 0L) - s);
+            long area = s * s;
+
+            total += area;
+            events.add(new long[]{y, s});
+            events.add(new long[]{y + s, -s});
         }
 
-        long[] curr = new long[diff.size()];
-        int idx = 0;
-        for (long k : diff.keySet()) curr[idx++] = k;
-        Arrays.sort(curr);
+        events.sort(Comparator.comparingLong(a -> a[0]));
 
-        long area = 0;
-        long target = 0;
+        long currWidth = 0;
+        long accumulated = 0;
 
-        for (int i = 0; i + 1 < curr.length; i++) {
-            long y = curr[i];
-            long y2 = curr[i + 1];
+        for (int i = 0; i + 1 < events.size(); i++) {
+            currWidth += events.get(i)[1];
+            long y1 = events.get(i)[0];
+            long y2 = events.get(i + 1)[0];
 
-            target += diff.get(y);
-            area += target * (y2 - y);
+            long stripArea = currWidth * (y2 - y1);
 
-            if (area * 2L >= total) {
-                return (double)y2 - (double)(area * 2L - total) / (double)(target * 2L);
+            if ((accumulated + stripArea) * 2 >= total) {
+                long remaining = total - accumulated * 2;
+                return y1 + (double) remaining / (2.0 * currWidth);
             }
+
+            accumulated += stripArea;
         }
 
-        return (double)curr[curr.length - 1];
+        return events.get(events.size() - 1)[0];
     }
 }
