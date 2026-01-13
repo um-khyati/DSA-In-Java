43443bee16d3c08b33a2dd34fcb02b46d34323ad
Khyati Uttam <ukhyati05@gmail.com>
2026-01-13 20:03:03 +0530

    Time: 29 ms (90.87%) | Memory: 59.2 MB (49.74%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -11,14 +11,22 @@ class Solution {
             int j = i + 1, k = n - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
+
                 if (sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
-                } else if (sum < 0) j++;
-                else k--;
+                } 
+                else if (sum < 0) {
+                    j++;
+                } 
+                else {
+                    k--;
+                }
             }
         }
         return res;
