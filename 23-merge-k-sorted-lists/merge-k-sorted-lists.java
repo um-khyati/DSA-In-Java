fc7e775c6939116ee08b67f52be9384be6e2bf51
Khyati Uttam <ukhyati05@gmail.com>
2026-01-13 20:08:02 +0530

    Time: 1 ms (99.66%) | Memory: 47.1 MB (8.27%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,32 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.List;
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
-    }
-
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        return divideAndConquer(lists, 0, lists.length - 1);
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeKListsHelper(lists, 0, lists.length - 1);
     }
-
-    private ListNode divideAndConquer(ListNode[] lists, int left, int right) {
-        if (left == right) return lists[left];
-
-        int mid = left + (right - left) / 2;
-        ListNode l1 = divideAndConquer(lists, left, mid);
-        ListNode l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+    
+    private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        if (start + 1 == end) {
+            return merge(lists[start], lists[end]);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        curr.next = (l1 != null) ? l1 : l2;
+        
+        return dummy.next;
     }
 }
