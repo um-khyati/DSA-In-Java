58c9c01c904ffcc291565ef5f15e051cdbe8ab20
Khyati Uttam <ukhyati05@gmail.com>
2026-01-15 20:31:21 +0530

    Time: 5 ms (60.29%) | Memory: 45.2 MB (89.71%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.java
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.java
@@ -1,39 +1,17 @@
 class Solution {
+    private int maxLen(int[] Bars) {
+        int count = 2, length = 2;
+        for(int i = 1; i < Bars.length; i++) {
+            if(Bars[i] - Bars[i-1] == 1) count++;
+            else count = 2;
+            length = Math.max(length, count);
+        }
+        return length;
+    }
     public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
-        int maxConsecutiveHBars = 1;
-        int maxConsecutiveVBars = 1;
-
         Arrays.sort(hBars);
         Arrays.sort(vBars);
-
-        int hLength = hBars.length;
-        int vLength = vBars.length;
-
-        int tempH = 1;
-        int tempV = 1;
-
-        for (int i = 1; i < Math.max(hLength, vLength); i++) {
-
-            if (i < hLength && hBars[i] - hBars[i - 1] == 1) {
-                tempH++;
-            } else if (i < hLength) {
-                maxConsecutiveHBars = Math.max(maxConsecutiveHBars, tempH);
-                tempH = 1;
-            }
-
-            if (i < vLength && vBars[i] - vBars[i - 1] == 1) {
-                tempV++;
-            } else if (i < vLength) {
-                maxConsecutiveVBars = Math.max(maxConsecutiveVBars, tempV);
-                tempV = 1;
-            }
-        }
-
-        maxConsecutiveHBars = Math.max(maxConsecutiveHBars, tempH);
-        maxConsecutiveVBars = Math.max(maxConsecutiveVBars, tempV);
-
-        int squareLen = Math.min(maxConsecutiveHBars, maxConsecutiveVBars) + 1;
-
-        return squareLen * squareLen;
+        int side = Math.min(maxLen(hBars), maxLen(vBars));
+        return side * side; 
     }
 }
