a7dce678c9780a9bc76c8082905ae75d1a908f95
Khyati Uttam <ukhyati05@gmail.com>
2026-01-16 19:50:39 +0530

    Time: 3 ms (92.38%) | Memory: 44.6 MB (95.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,43 +1,16 @@
 class Solution {
-    int n1;
-    int n2;
-
-    // Next Greater Element to the Right for nums2
-    public int[] getNGR(int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        int[] NGR = new int[n2];
-
-        for (int i = n2 - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums2[i] >= nums2[stack.peek()]) {
-                stack.pop();
-            }
-
-            NGR[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
-        }
-        return NGR;
-    }
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        n1 = nums1.length;
-        n2 = nums2.length;
-
-        int[] NGR = getNGR(nums2);
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // Map each element in nums2 to its next greater element
-        for (int i = 0; i < n2; i++) {
-            if (NGR[i] == -1) {
-                map.put(nums2[i], -1);
-            } else {
-                map.put(nums2[i], nums2[NGR[i]]);
+        int[] result = new int[nums1.length];
+        int j = 0;
+        for(int num : nums1){
+            int max = -1;
+            int i = 0;
+            while(nums2[i++] != num);
+            while(i < nums2.length && nums2[i] <= num){
+                i++;
             }
-        }
-
-        // Build result for nums1
-        int[] result = new int[n1];
-        for (int i = 0; i < n1; i++) {
-            result[i] = map.get(nums1[i]);
+            max = (i == nums2.length) ? -1 : nums2[i];
+            result[j++] = max;
         }
         return result;
     }
