f25da615b536cfbd4878f3c71201b16831500946
Khyati Uttam <ukhyati05@gmail.com>
2026-01-16 19:40:44 +0530

    Time: 416 ms (80.95%) | Memory: 166.5 MB (63.09%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.java
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.java
@@ -1,36 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
-        List<Integer> hList = new ArrayList<>();
-        for (int h : hFences) hList.add(h);
-        hList.add(1);
-        hList.add(m);
+      
+        int[] h = Arrays.copyOf(hFences, hFences.length + 2);
+        h[hFences.length] = 1;
+        h[hFences.length + 1] = m;
+        
+        int[] v = Arrays.copyOf(vFences, vFences.length + 2);
+        v[vFences.length] = 1;
+        v[vFences.length + 1] = n;
 
-        List<Integer> vList = new ArrayList<>();
-        for (int v : vFences) vList.add(v);
-        vList.add(1);
-        vList.add(n);
-        Set<Integer> stt = new HashSet<>();
-        long ans = 0;
-        for (int i = 0; i < hList.size(); i++) {
-            for (int j = i + 1; j < hList.size(); j++) {
-                stt.add(Math.abs(hList.get(j) - hList.get(i)));
+        Arrays.sort(h);
+        Arrays.sort(v);
+
+        Set<Integer> hGaps = new HashSet<>();
+        for (int i = 0; i < h.length; i++) {
+            for (int j = i + 1; j < h.length; j++) {
+                hGaps.add(h[j] - h[i]);
             }
         }
-        for (int i = 0; i < vList.size(); i++) {
-            for (int j = i + 1; j < vList.size(); j++) {
-                int val = Math.abs(vList.get(j) - vList.get(i));
-                if (stt.contains(val)) {
-                    ans = Math.max(ans, val);
+
+        long maxSide = -1;
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < v.length; j++) {
+                int currentGap = v[j] - v[i];
+                if (hGaps.contains(currentGap)) {
+                    maxSide = Math.max(maxSide, currentGap);
                 }
             }
         }
-        if (ans == 0) {
-            return -1;
-        }
 
+        if (maxSide == -1) return -1;
         long MOD = 1_000_000_007;
-        return (int) ((ans * ans) % MOD);
+        return (int) ((maxSide * maxSide) % MOD);
     }
 }
