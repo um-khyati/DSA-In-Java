230c10569f433330fdd0752b951472e51836680e
Khyati Uttam <ukhyati05@gmail.com>
2026-01-16 19:47:09 +0530

    Time: 3 ms (87.68%) | Memory: 43.7 MB (12.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,13 +1,16 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') stack.push(')');
-            else if (ch == '[') stack.push(']');
-            else if (ch == '{') stack.push('}');
-            else {
-                if (stack.isEmpty() || stack.pop() != ch) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || 
+                   (c == ')' && stack.pop() != '(') || 
+                   (c == '}' && stack.pop() != '{') || 
+                   (c == ']' && stack.pop() != '[')) {
                     return false;
                 }
             }
