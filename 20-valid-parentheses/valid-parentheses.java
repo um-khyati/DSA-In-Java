7e9237b5813e1d831b33492cc861c43f90c8f95d
Khyati Uttam <ukhyati05@gmail.com>
2026-01-16 19:45:35 +0530

    Time: 3 ms (87.68%) | Memory: 42.9 MB (96.75%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
+
         for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
+            if (ch == '(') stack.push(')');
+            else if (ch == '[') stack.push(']');
+            else if (ch == '{') stack.push('}');
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) {
                     return false;
                 }
             }
