c22a9293e4186c1abd1e24150c2915319ad0c584
Khyati Uttam <ukhyati05@gmail.com>
2026-01-16 19:47:39 +0530

    Time: 1 ms (99.67%) | Memory: 42.9 MB (93.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,19 +2,22 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty() || 
-                   (c == ')' && stack.pop() != '(') || 
-                   (c == '}' && stack.pop() != '{') || 
-                   (c == ']' && stack.pop() != '[')) {
-                    return false;
-                }
+        int len = s.length();
+        if((len & 1) == 1) return false;
+
+        char[] stck = new char[len];
+        int top = 0;
+
+        for(int i = 0; i < len; i++){
+            char ch = s.charAt(i);
+            switch(ch){
+                case '(': stck[top++] = ')'; break;
+                case '[': stck[top++] = ']'; break;
+                case '{': stck[top++] = '}'; break;
+                default:
+                    if(top == 0 || stck[--top] != ch) return false;
             }
         }
-        return stack.isEmpty();
+        return top == 0;
     }
 }
