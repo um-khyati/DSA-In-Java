42af7c45adcf7a3c68d6a15128f228f3a1604ec1
Khyati Uttam <ukhyati05@gmail.com>
2026-01-19 19:30:59 +0530

    Time: 6 ms (87.31%) | Memory: 57.7 MB (46.19%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,36 +1,26 @@
 class Solution {
-    public int maxSideLength(int[][] mat, int threshold) {
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        int[][] P = new int[m + 1][n + 1];
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                P[i][j] = mat[i-1][j-1] + P[i-1][j] + P[i][j-1] - P[i-1][j-1];
-            }
+  public int maxSideLength(int[][] mat, int threshold) {
+    final int m = mat.length;
+    final int n = mat[0].length;
+    int ans = 0;
+    int[][] prefix = new int[m + 1][n + 1];
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        prefix[i + 1][j + 1] = mat[i][j] + prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j];
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        for (int length = ans; length < Math.min(m - i, n - j); ++length) {
+          if (squareSum(prefix, i, j, i + length, j + length) > threshold)
+            break;
+          ans = Math.max(ans, length + 1);
         }
-        
-        int maxSide = 0;
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int len = maxSide + 1;
-                
-                if (i >= len && j >= len) {
-                    int r1 = i - len + 1;
-                    int c1 = j - len + 1;
-                    
-                    // Calculate sum of square defined by (r1, c1) and (i, j)
-                    int total = P[i][j] - P[r1-1][j] - P[i][c1-1] + P[r1-1][c1-1];
-                    
-                    if (total <= threshold) {
-                        maxSide++;
-                    }
-                }
-            }
-        }
-        
-        return maxSide;
-    }
+
+    return ans;
+  }
+
+  private int squareSum(int[][] prefix, int r1, int c1, int r2, int c2) {
+    return prefix[r2 + 1][c2 + 1] - prefix[r1][c2 + 1] - prefix[r2 + 1][c1] + prefix[r1][c1];
+  }
 }
