49c5e05a7b9d5c454bae09cf9c8c10803861bcfe
Khyati Uttam <ukhyati05@gmail.com>
2026-01-26 19:56:37 +0530

    Time: 9 ms (99.64%) | Memory: 63.3 MB (98.11%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,19 +1,46 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        Arrays.sort(arr);
-        List<List<Integer>> result = new ArrayList<>();
-        int minDiff = Integer.MAX_VALUE;
         
-        for (int i = 0; i < arr.length - 1; i++) {
-            int currentDiff = arr[i + 1] - arr[i];
-            if (currentDiff < minDiff) {
-                minDiff = currentDiff;
-                result.clear();
-                result.add(Arrays.asList(arr[i], arr[i + 1]));
-            } else if (currentDiff == minDiff) {
-                result.add(Arrays.asList(arr[i], arr[i + 1]));
+        //Counting sort
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int num : arr){
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        boolean[] nums = new boolean[max - min + 1];
+
+        //sorted in counting
+        for(int n : arr){
+            nums[n - min] = true;
+        }
+
+        int[] newArr = new int[arr.length];
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] ){
+                newArr[j] = i + min;
+                j++;
             }
         }
-        return result;
+
+        int absMin = Integer.MAX_VALUE;
+        for(int i = 0; i < newArr.length - 1; i++){
+            int temp = newArr[i+1] - newArr[i];
+            if(temp < absMin){
+                absMin = temp;
+            }
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < newArr.length - 1; i++){
+            int temp = newArr[i+1] - newArr[i];
+            if(temp == absMin){
+                res.add(List.of(newArr[i], newArr[i+1]));
+            }
+        }
+
+        return res;
     }
 }
