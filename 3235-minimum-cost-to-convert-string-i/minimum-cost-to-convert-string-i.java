bd4943f1d1b06fdc6b2c79ba571e567d433cb82c
Khyati Uttam <ukhyati05@gmail.com>
2026-01-29 14:08:23 +0530

    Time: 12 ms (100.00%) | Memory: 48.3 MB (16.49%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,46 +1,37 @@
-import java.util.Arrays;
-
 class Solution {
-    public long minimumCost(String source, String target, 
-char[] original, char[] changed, int[] cost) {
-        long[][] dist = new long[26][26];
-        long INF = Long.MAX_VALUE / 2;
-
-        for (long[] row : dist) {
-            Arrays.fill(row, INF);
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        int[][] dis = new int[26][26];
+        for(int i = 0 ; i < 26 ;i++){
+            Arrays.fill(dis[i] , Integer.MAX_VALUE);
+            dis[i][i] = 0 ;
         }
-        for (int i = 0; i < 26; i++) {
-            dist[i][i] = 0;
-        }
-
-        for (int i = 0; i < original.length; i++) {
-            int u = original[i] - 'a';
-            int v = changed[i] - 'a';
-            dist[u][v] = Math.min(dist[u][v], cost[i]);
+        
+        for(int i = 0 ; i < cost.length ; i++){
+            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'],cost[i]);
         }
 
-        for (int k = 0; k < 26; k++) {
-            for (int i = 0; i < 26; i++) {
-                if (dist[i][k] == INF) continue;
-                for (int j = 0; j < 26; j++) {
-                    if (dist[k][j] != INF) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        for(int k = 0 ; k < 26 ; k++){
+            for(int i = 0 ; i < 26 ; i++){
+                if(dis[i][k] < Integer.MAX_VALUE){
+                    for(int j = 0 ; j < 26 ; j++){
+                        if(dis[k][j] < Integer.MAX_VALUE){
+                            dis[i][j] = Math.min(dis[i][j] , dis[i][k] + dis[k][j]);
+                        }
                     }
                 }
             }
         }
 
-        long totalCost = 0;
-        int n = source.length();
-
-        for (int i = 0; i < n; i++) {
-            int u = source.charAt(i) - 'a';
-            int v = target.charAt(i) - 'a';
-            if (u == v) continue;
-            if (dist[u][v] == INF) return -1;
-            totalCost += dist[u][v];
+        long ans = 0L ;
+        for(int i = 0 ; i <source.length() ;i++){
+            int c1 = source.charAt(i) - 'a';
+            int c2 = target.charAt(i) - 'a';
+            if(dis[c1][c2] == Integer.MAX_VALUE){
+                return -1L;
+            }
+            ans += (long)dis[c1][c2];
         }
 
-        return totalCost;
+        return ans ;
     }
 }
