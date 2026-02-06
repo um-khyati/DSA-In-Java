9fc00e2ff7dc0e35a5112aa970bcc9116601e48f
Khyati Uttam <ukhyati05@gmail.com>
2026-02-06 20:34:44 +0530

    Time: 1 ms (98.53%) | Memory: 45.5 MB (5.35%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,17 +1,35 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> res = new HashSet<>();
-
-        for (int n : nums1) set.add(n);
-        for (int n : nums2)
-            if (set.contains(n)) res.add(n);
-
-        int[] ans = new int[res.size()];
-        int i = 0;
-        for (int n : res) ans[i++] = n;
-
-        return ans;
+        int size = 2048;
+        int mask = size - 1;
+        int[] table = new int[size];
+        boolean[] occupied = new boolean[size];
+        for (int num : nums1) {
+            int h = Integer.hashCode(num) & mask;
+            while (occupied[h] && table[h] != num) {
+                h = (h + 1) & mask;
+            }
+            table[h] = num;
+            occupied[h] = true;
+        }
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int count = 0;
+        boolean[] intersectionSet = new boolean[size];
+        for (int num : nums2) {
+            int h = Integer.hashCode(num) & mask;
+            while (occupied[h]) {
+                if (table[h] == num) {
+                    if (!intersectionSet[h]) {
+                        temp[count++] = num;
+                        intersectionSet[h] = true;
+                    }
+                    break;
+                }
+                h = (h + 1) & mask;
+            }
+        }
+        int[] result = new int[count];
+        System.arraycopy(temp, 0, result, 0, count);
+        return result;
     }
 }
