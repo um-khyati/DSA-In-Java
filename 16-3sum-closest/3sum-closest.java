afb716c95ce06b68c496de2c212fb86484fb22cc
Khyati Uttam <ukhyati05@gmail.com>
2026-02-06 20:33:30 +0530

    Time: 7 ms (99.48%) | Memory: 45.5 MB (62.61%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -2,24 +2,41 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int n = nums.length;
-        int result = nums[0] + nums[1] + nums[2]; 
-
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (Math.abs(target - sum) < Math.abs(target - result)) {
-                    result = sum;
+        int res = 0;
+        int abs = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length - 2; i++){
+            if(nums[i] + nums[n - 1] + nums[n - 2] < target){
+                if(Math.abs(nums[i] + nums[n - 1] + nums[n - 2] - target) < abs){
+                    abs = Math.abs(nums[i] + nums[n - 1] + nums[n - 2] - target);
+                    res = nums[i] + nums[n - 1] + nums[n - 2];
                 }
-
-                if (sum == target) return target;
-                else if (sum < target) left++;
-                else right--;
+                continue;
             }
+            if(nums[i] + nums[i + 1] + nums[i + 2] > target){
+                if(Math.abs(nums[i] + nums[i + 1] + nums[i + 2] - target) < abs){
+                    abs = Math.abs(nums[i] + nums[i + 1] + nums[i + 2] - target);
+                    res = nums[i] + nums[i + 1]+ nums[i + 2];
+                }
+                break;
+            }
+            int left = i + 1, right = n - 1;
+            while(left < right){
+                int currentSum = nums[i] + nums[left] + nums[right];
+                if(currentSum == target){
+                    return target;
+                }
+                if(Math.abs(currentSum - target) < abs){
+                    res = currentSum;
+                    abs = Math.abs(currentSum - target);
+                }
+                if(currentSum > target){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+            
         }
-
-        return result;
+        return res;
     }
 }
