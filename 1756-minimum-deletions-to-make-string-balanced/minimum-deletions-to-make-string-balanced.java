74ab4033303e1308026a8757856c63575f9911f0
Khyati Uttam <ukhyati05@gmail.com>
2026-02-07 19:49:19 +0530

    Time: 18 ms (100.00%) | Memory: 48 MB (30.43%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int res = 0;
+        int count = 0;
+
+        for(char c : s.toCharArray()) {
+            if ( c == 'b' ){
+                count++;
+            } else if ( count!=0 ) {
+                res++;
+                count--;
+            }
+        }
+        return res;
+    }
+}
