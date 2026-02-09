237508f466867b99703ef53eea50a3cd81cb3e5d
Khyati Uttam <ukhyati05@gmail.com>
2026-02-09 23:33:26 +0530

    Time: 2 ms (60.59%) | Memory: 42.4 MB (21.15%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+
+        backtrack(0, board, n);
+        return count;
+    }
+
+    private void backtrack(int row, char[][] board, int n) {
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int col, char[][] board, int n) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+}
