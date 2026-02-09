941f4bfda6e4bf3b1d64fd82a92f788e9ca55e7d
Khyati Uttam <ukhyati05@gmail.com>
2026-02-09 23:31:12 +0530

    Time: 3 ms (45.75%) | Memory: 46.8 MB (43.43%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,51 +1,48 @@
+import java.util.*;
+
 class Solution {
-    static boolean isSafe(int n,int c, int r,  char [][] board){
-       for (int i = 0; i < r; i++) {
-            if (board[i][c] == 'Q')
-                return false;
-        }
+    List<List<String>> ans = new ArrayList<>();
 
-        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-
- 
-        for (int i = r, j = c; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-    
-    return true;
-    }
-    public static void solve(int n, char [][] board , List<List<String>> ans,int r){
-if(r==n){
-ArrayList<String> a= new ArrayList<>();
-
-    for(int i=0; i<n; i++){
-       a.add(new String(board[i]));
-    }
-  ans.add(new ArrayList<>(a));
-    return;
-}
-
-        for(int i=0; i<n; i++){
-if(isSafe(n,i,r,board)){
-    board[r][i]='Q';
-    
-    solve(n,board,ans,r+1);
-    board[r][i]='.';
-    
-}
-        }
-    }
     public List<List<String>> solveNQueens(int n) {
-           char [][] board =  new char[n][n];
-        for(int i=0; i<n; i++){
-            Arrays.fill(board[i],'.');
-        }
-List<List<String>> ans = new ArrayList<>();
-        solve(n,board, ans,0);
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(board[i], '.');
+
+        backtrack(0, board, n);
         return ans;
+    }
+
+    private void backtrack(int row, char[][] board, int n) {
+        if (row == n) {
+            List<String> temp = new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                temp.add(new String(board[i]));
+            ans.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int col, char[][] board, int n) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
     }
 }
