38ecd7d91adc80b8cbdde5696e0c6af1f83a1e12
Khyati Uttam <ukhyati05@gmail.com>
2026-02-11 19:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (43.06%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int n = arr1.length ,m =arr2.length ,max = 0;
-        for(int i : arr1){
-            max = Math.max(max,i);
+
+       
+        int[] count = new int[1001];
+        for (int i = 0; i < arr1.length; i++) {
+            count[arr1[i]]++;
         }
 
-        int[] countArr = new int[max+1];
-
-        for(int i : arr1){
-            countArr[i]++;
+        int index = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            int num = arr2[i];
+            for (int j = 0; j < count[num]; j++) {
+                arr1[index] = num;
+                index++;
+            }
+            count[num] = 0; 
         }
 
-        int idx = 0;
-        int[] result = new int[n];
-
-        for(int i : arr2){
-            while(countArr[i]>0){
-                result[idx++] = i;
-                countArr[i]--;
+        
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                arr1[index] = i;
+                index++;
             }
         }
-        for(int i = 0; i< countArr.length ; i++){
-            while(countArr[i]>0){
-                result[idx++] = i;
-                countArr[i]--;
-            }
-        }
-        return result;
+
+        return arr1;
     }
 }
