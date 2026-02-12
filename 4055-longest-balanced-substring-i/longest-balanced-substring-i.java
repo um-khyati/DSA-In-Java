2172659ea6a4b3434abb63156d55acd63a678723
Khyati Uttam <ukhyati05@gmail.com>
2026-02-12 19:31:04 +0530

    Time: 21 ms (100.00%) | Memory: 47 MB (42.88%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -1,34 +1,29 @@
 class Solution {
     public int longestBalanced(String s) {
-        int max = 0;
         int n = s.length();
-        
-        for(int i = 0; i < n; i++){
-            int freq[] = new int[26];
-            
-            for(int j = i; j < n; j++){
-                char ch = s.charAt(j);
-                freq[ch - 'a']++;
-                
-                if(issamefreq(freq)){
-                    max = Math.max(max, j - i + 1);
-                }
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) 
+            a[i] = s.charAt(i) - 'a';
+        int result = 0;
+        for (int l = 0; l < n; l++) {
+            if (n - l <= result) 
+                break;
+
+            int[] cnt = new int[26]; 
+            int uniq = 0, maxfreq = 0; 
+            for (int r = l; r < n; r++) {
+                int i = a[r];
+                if (cnt[i] == 0) 
+                    uniq++;
+
+                cnt[i]++;
+                if (cnt[i] > maxfreq) 
+                    maxfreq = cnt[i];
+                int cur = r - l + 1;
+                if (uniq * maxfreq == cur && cur > result)
+                    result = cur;
             }
         }
-        return max;
-    }
-    
-    public static boolean issamefreq(int arr[]){
-        int value = 0;
-        
-        for(int a : arr){
-            if(a == 0) continue;
-            
-            if(value == 0) 
-                value = a;
-            else if(value != a) 
-                return false;
-        }
-        return true;
+        return result;
     }
 }
