07216fbbc66aee13daa432b18f854ccf827bb708
Khyati Uttam <ukhyati05@gmail.com>
2026-02-16 19:20:26 +0530

    Time: 4 ms (99.28%) | Memory: 45 MB (93.70%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,22 +1,46 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-    
-}
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+      bubbleSort(candidates);
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{
-        for(int i = start; i < nums.length; i++){
-            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i + 1);
-            tempList.remove(tempList.size() - 1); 
+      List<List<Integer>> list = new ArrayList<>();
+
+      backtrack(list, new ArrayList<>(), candidates, target, 0);
+      
+      return list;
+    }
+
+    public static void backtrack(List<List<Integer>> list, List<Integer> temp, int[] arr, int tar, int start){
+
+        if(tar == 0) {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+
+        if(tar < 0){
+            return;
+        }
+
+        for(int i = start; i < arr.length; i++){
+            if(i > start && arr[i] == arr[i-1]){
+                continue;
+            }
+            temp.add(arr[i]);
+            backtrack(list, temp, arr, tar - arr[i], i+1 );
+            temp.remove(temp.size() - 1);
         }
     }
-} 
+
+
+     private static void bubbleSort(int[] arr){
+        int n = arr.length;
+        for ( int i = 0; i < n - 1; i++){
+            for ( int j = 0; j < n - i - 1; j++){
+                if ( arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
