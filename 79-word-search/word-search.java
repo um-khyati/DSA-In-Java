ff950810f4beb857f3e0478dec30506211f9ac89
Khyati Uttam <ukhyati05@gmail.com>
2026-02-17 23:36:58 +0530

    Time: 169 ms (28.15%) | Memory: 42.8 MB (89.29%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,56 +1,55 @@
 class Solution {
-    private static int[][] DIRS = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    private boolean isValid(int i, int j, boolean[][] visited) {
-        return (
-            i >= 0 && i < visited.length &&
-            j >= 0 && j < visited[0].length &&
-            !visited[i][j]
-        );
-    }
+    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
 
     public boolean exist(char[][] board, String word) {
-        if (
-            board == null ||
-            board.length == 0 || board[0].length == 0
-        ) return (word.length() == 0);
 
-        if (board.length * board[0].length < word.length()) return false;
+        int m = board.length, n = board[0].length;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                boolean[][] visited = new boolean[board.length][board[0].length];        
-                int wordIndex = 0; 
-                if (dfs(i, j, board, visited, word, wordIndex))
-                    return true;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] == word.charAt(0)) {
+
+                    boolean[][] visited = new boolean[m][n];
+
+                    if (dfs(i, j, 0, board, word, visited))
+                        return true;
+                }
             }
         }
 
         return false;
     }
 
-    private boolean dfs(
-        int i, int j, 
-        char[][] board, boolean[][] visited,
-        String word, int wordIndex) {
+    private boolean dfs(int r, int c, int idx,
+                        char[][] grid,
+                        String word,
+                        boolean[][] visited) {
 
-        if (visited[i][j]) return false;
-        visited[i][j] = true;
-        if (board[i][j] != word.charAt(wordIndex++)) {
-            visited[i][j] = false;
+        if (grid[r][c] != word.charAt(idx))
             return false;
-        }
-        if (wordIndex == word.length()) return true;
-        for (int[] dir: DIRS) {
-            int newI = i + dir[0];
-            int newJ = j + dir[1];
 
-            if (isValid(newI, newJ, visited)) {
-                if (dfs(newI, newJ, board, visited, word, wordIndex))
+        if (idx == word.length() - 1)
+            return true;
+
+        visited[r][c] = true;
+
+        for (int[] d : dirs) {
+            int x = r + d[0];
+            int y = c + d[1];
+
+            if (x >= 0 && y >= 0 &&
+                x < grid.length && y < grid[0].length &&
+                !visited[x][y]) {
+
+                if (dfs(x, y, idx + 1, grid, word, visited))
                     return true;
             }
         }
-        visited[i][j] = false;
+
+        visited[r][c] = false;
+
         return false;
     }
 }
