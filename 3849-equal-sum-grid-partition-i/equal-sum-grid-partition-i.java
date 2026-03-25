ec70096fdd6f94ec40787643ba91c39435b8d5b5
Khyati Uttam <ukhyati05@gmail.com>
2026-03-25 18:55:35 +0530

    Time: 4 ms (97.20%) | Memory: 159.8 MB (95.10%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,48 +1,47 @@
 class Solution {
-    int n, m;
-
-    boolean checkHz(int[][] grid){
-        HashSet<Long> st = new HashSet<>();
-
-        long pref = 0;
-
-        for(int i = 0; i < n; i++){
-            long rowSum = 0;
-            for(int j = 0; j < m; j++){
-                rowSum += grid[i][j];
-            }
-            pref += rowSum;
-            st.add(pref);
-        }
-
-        if(pref % 2 != 0) return false;
-
-        return st.contains(pref / 2);
-    }
-
-    boolean checkVz(int[][] grid){
-        HashSet<Long> st = new HashSet<>();
-
-        long pref = 0;
-
-        for(int j = 0; j < m; j++){
-            long colSum = 0;
-            for(int i = 0; i < n; i++){
-                colSum += grid[i][j];
-            }
-            pref += colSum;
-            st.add(pref);
-        }
-
-        if(pref % 2 != 0) return false;
-
-        return st.contains(pref / 2);
-    }
-
     public boolean canPartitionGrid(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
+        int n=grid.length,m=grid[0].length;
+        long hor[]= new long[n];
+        long total1=0;
+        for(int i=0;i<n;i++)
+        {
+            long temp=0;
+            for(int j=0;j<m;j++)
+            {
+                int t=grid[i][j];
+                 temp+=t;
+                 
+            }
+            hor[i]=temp+total1;
+            total1+=temp;
+        }
+       
+        for(int i=0;i<n;i++)
+        {
+            if(hor[i]==total1-hor[i])
+            return true;
+        }
 
-        return checkHz(grid) || checkVz(grid);
+        long ver[]= new long[m];
+        long total2=0;
+        for(int j=0;j<m;j++)
+        {
+            long temp=0;
+            for(int i=0;i<n;i++)
+            {
+                temp+=grid[i][j];
+            }
+            ver[j]=temp+total2;
+            total2+=temp;
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            if(ver[i]==total2-ver[i])
+            return true;
+        }
+
+        return false;
+
     }
 }
