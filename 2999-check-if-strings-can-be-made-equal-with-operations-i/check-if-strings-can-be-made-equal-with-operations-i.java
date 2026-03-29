4572d01f20bc11e1780c1d3431f9c76a1f282992
Khyati Uttam <ukhyati05@gmail.com>
2026-03-29 14:49:07 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (5.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,28 +1,9 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        char[] even1 = new char[2], odd1 = new char[2];
-        char[] even2 = new char[2], odd2 = new char[2];
-        
-        int e = 0, o = 0;
-        
-        for (int i = 0; i < 4; i++) {
-            if (i % 2 == 0) {
-                even1[e] = s1.charAt(i);
-                even2[e] = s2.charAt(i);
-                e++;
-            } else {
-                odd1[o] = s1.charAt(i);
-                odd2[o] = s2.charAt(i);
-                o++;
-            }
-        }
-        
-        java.util.Arrays.sort(even1);
-        java.util.Arrays.sort(even2);
-        java.util.Arrays.sort(odd1);
-        java.util.Arrays.sort(odd2);
-        
-        return java.util.Arrays.equals(even1, even2) && 
-               java.util.Arrays.equals(odd1, odd2);
+        if(s1.charAt(0)!=s2.charAt(0)&&(s1.charAt(0)!=s2.charAt(2)||s1.charAt(2)!=s2.charAt(0)))return false;
+        if(s1.charAt(1)!=s2.charAt(1)&&(s1.charAt(1)!=s2.charAt(3)||s1.charAt(3)!=s2.charAt(1)))return false;
+        if(s1.charAt(0)==s2.charAt(0)&&s1.charAt(2)!=s2.charAt(2))return false;
+        if(s1.charAt(1)==s2.charAt(1)&&s1.charAt(3)!=s2.charAt(3))return false;
+        return true;
     }
 }
