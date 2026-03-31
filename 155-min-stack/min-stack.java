b16c06222aba06cb6e1900fb646462d079a95e40
Khyati Uttam <ukhyati05@gmail.com>
2026-03-31 17:13:45 +0530

    Time: 4 ms (99.96%) | Memory: 46.9 MB (76.87%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,36 +1,50 @@
 class MinStack {
-    private Stack<Integer> stack;
-    private Stack<Integer> minStack;
-
+    static ListNode head;
+    static int min;
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
+        head = null;
+        min = Integer.MAX_VALUE;
     }
-    
+
     public void push(int val) {
-        stack.push(val);
-        
-        if (minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
-        } else {
-            minStack.push(minStack.peek());
+
+        if(min > val){
+            min = val;
         }
+        head = new ListNode(val, min, head);
     }
-    
+
     public void pop() {
-        stack.pop();
-        minStack.pop();
+        head = head.next;
+        if(head == null){min = Integer.MAX_VALUE;}
+        else {min = head.currentMin;}
     }
-    
+
     public int top() {
-        return stack.peek();
+        return head.val;
     }
-    
+
     public int getMin() {
-        return minStack.peek();
+        return head.currentMin;
     }
 }
+class ListNode {
+    int      val;
+    int      currentMin;
+    ListNode next;
 
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, int currentMin, ListNode next) {
+        this.currentMin = currentMin;
+        this.val = val;
+        this.next = next;
+    }
+}
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
