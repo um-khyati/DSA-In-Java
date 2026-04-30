54e8abebe2ccebffa2b679ddb999aed9b0b7e53e
Khyati Uttam <ukhyati05@gmail.com>
2026-05-01 00:11:17 +0530

    Time: 186 ms (95.70%) | Memory: 86.2 MB (59.14%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.java
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.java
@@ -1,25 +1,52 @@
 class Solution {
-    int limit;
     public int maxPathScore(int[][] grid, int k) {
-        limit=-2;
-        int m=grid.length,n=grid[0].length;
-        int[][][] dp=new int[m+1][n+1][Math.min(k+1,m+n+1)];
-        for(int[][] i:dp) for(int j[]:i) Arrays.fill(j,-1);
-        int ans=compute(0,0,0,grid,k,dp);
-        return ans==limit?-1:ans;
-    }
-    public int compute(int i,int j,int cost,int[][] grid,int k,int[][][] dp){
-        if(cost>k) return limit;
-        if(dp[i][j][cost]!=-1) return dp[i][j][cost];
-        if(i==grid.length-1&&j==grid[0].length-1){
-            if(grid[i][j]!=0) if(cost+1>k) return limit;
-            return grid[i][j];
+        int m = grid.length, n = grid[0].length;
+        int[][][] dp = new int[m][n][k+1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int[] d = dp[i][j];
+                Arrays.fill(d, -1);
+            }
         }
-        if(i==grid.length||j==grid[0].length) return limit;
-        int ans=limit;
-        int toadd=(grid[i][j]!=0)?1:0;
-        ans=Math.max(ans,compute(i+1,j,cost+toadd,grid,k,dp));
-        ans=Math.max(ans,compute(i,j+1,cost+toadd,grid,k,dp));
-        return dp[i][j][cost]=(ans==limit?ans:ans+grid[i][j]);
+        int zero = grid[0][0];
+        dp[0][0][zero] = grid[0][0];
+        int total = grid[0][0] != 0 ? 1 : 0;
+        for (int i = 1; i < m; i++) {
+            int score = grid[i][0];
+            int cost = score != 0 ? 1 : 0;
+            if (total+cost >k) break;
+            dp[i][0][total+cost] = dp[i-1][0][total] + score;
+            total += cost;
+        }
+        total = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            int score = grid[0][j];
+            int cost = score != 0 ? 1 : 0;
+            if (total+cost >k) break;
+            dp[0][j][total+cost] = dp[0][j-1][total] + score;
+            total += cost;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+               
+                int score = grid[i][j];
+                int cost = score != 0 ? 1 : 0;
+                if (cost > k) break;
+                for (int s = k; s >= cost; s--) {
+                    if (dp[i][j-1][s-cost] == -1 && 
+                    dp[i-1][j][s-cost] == -1) continue;
+                    dp[i][j][s] = Math.max(dp[i][j-1][s-cost],  dp[i-1][j][s-cost]) + score; 
+                }
+            }
+        }
+
+        int rets = -1;
+        for (int i = 0; i <= k; i++) {
+            rets = Math.max(rets, dp[m-1][n-1][i]);
+        }
+        return rets;
     }
 }
+
