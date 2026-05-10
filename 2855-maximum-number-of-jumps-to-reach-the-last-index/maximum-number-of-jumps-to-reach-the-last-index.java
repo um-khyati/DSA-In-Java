66f58d577442828708207fcba7407e056ae95c6f
Khyati Uttam <ukhyati05@gmail.com>
2026-05-10 09:03:34 +0530

    Time: 14 ms (80.95%) | Memory: 47 MB (32.14%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && dp[i] <= 0) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) <= Math.abs(target)) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
