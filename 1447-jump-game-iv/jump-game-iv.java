c445b47d2d6094c35bd2e2a077f85ab4d3500818
Khyati Uttam <ukhyati05@gmail.com>
2026-05-18 10:59:59 +0530

    Time: 46 ms (94.63%) | Memory: 75.8 MB (90.46%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,48 +1,55 @@
 class Solution {
-    public int minJumps(int[] nums) {
-        int n=nums.length;
+    public int minJumps(int[] arr) {
+        int n = arr.length;
 
-        Map<Integer,List<Integer>> map=new HashMap<>();
+        if (n == 1) return 0;
 
-        for(int i=0;i<n;i++){
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
         }
-        Queue<Integer> queue=new ArrayDeque<>();
-        boolean[] vis=new boolean[n];
 
-        queue.offer(0);
-        vis[0]=true;
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] vis = new boolean[n];
 
-        int steps=0;
+        q.offer(0);
+        vis[0] = true;
 
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                int idx=queue.poll();
-                if(idx==n-1){
-                    return steps;
+        int steps = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                int i = q.poll();
+
+                if (i == n - 1) return steps;
+
+                if (i + 1 < n && !vis[i + 1]) {
+                    vis[i + 1] = true;
+                    q.offer(i + 1);
                 }
-                if(idx-1>=0 && !vis[idx-1]){
-                    queue.offer(idx-1);
-                    vis[idx-1]=true;
+
+                if (i - 1 >= 0 && !vis[i - 1]) {
+                    vis[i - 1] = true;
+                    q.offer(i - 1);
                 }
-                if(idx+1<n && !vis[idx+1]){
-                    queue.offer(idx+1);
-                    vis[idx+1]=true;
-                }
-                if(map.containsKey(nums[idx])){
-                    for(int num:map.get(nums[idx])){
-                        if(!vis[num]){
-                            queue.offer(num);
-                            vis[num]=true;
+
+                List<Integer> next = map.remove(arr[i]);
+
+                if (next != null) {
+                    for (int idx : next) {
+                        if (!vis[idx]) {
+                            vis[idx] = true;
+                            q.offer(idx);
                         }
                     }
-                    map.remove(nums[idx]);
                 }
             }
-            steps++;
 
+            steps++;
         }
-        return steps;        
+        return -1;
     }
 }
