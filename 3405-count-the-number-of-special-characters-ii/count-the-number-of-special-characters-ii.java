5e8ae6772fd090ff3034a7bb3d68dcb6b9a824e3
Khyati Uttam <ukhyati05@gmail.com>
2026-05-27 11:14:36 +0530

    Time: 9 ms (80.58%) | Memory: 48 MB (64.88%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -1,45 +1,33 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[] lastLower = new int[26];
-        int[] firstUpper = new int[26];
 
-        Arrays.fill(lastLower, -1);
-        Arrays.fill(firstUpper, -1);
-
-        Set<Integer> invalid = new HashSet<>();
-
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                int idx = ch - 'a';
-
-                lastLower[idx] = i;
-
-                if (firstUpper[idx] != -1) {
-                    invalid.add(idx);
-                }
-
-            } else {
-                int idx = ch - 'A';
-
-                if (firstUpper[idx] == -1) {
-                    firstUpper[idx] = i;
+        int lowercase[]=new int[26];
+        int uppercase[]=new int[26];
+        Arrays.fill(lowercase,-1);
+        Arrays.fill(uppercase,-1);
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                lowercase[ch-'a']=i;
+            }
+            else{
+                int index= ch-'A';
+                if(uppercase[index]==-1)
+                {
+                    uppercase[index]=i;
                 }
             }
-        }
-
-        int specialCount = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (lastLower[i] != -1 &&
-                firstUpper[i] != -1 &&
-                !invalid.contains(i)) {
-
-                specialCount++;
+        }  
+        for(int i=0;i<26;i++)
+        {
+            if(lowercase[i]!=-1 && uppercase[i]!=-1 && lowercase[i]<uppercase[i])
+            {
+                count++;
             }
         }
-
-        return specialCount;
+        return count;
     }
 }
