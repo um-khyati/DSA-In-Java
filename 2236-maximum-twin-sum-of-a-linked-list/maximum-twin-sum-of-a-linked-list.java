4aeba73ccf80faf2e259958570a90e86cac98fab
Khyati Uttam <ukhyati05@gmail.com>
2026-06-14 11:13:55 +0530

    Time: 4 ms (98.30%) | Memory: 101.2 MB (57.49%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -9,18 +9,37 @@
  * }
  */
 class Solution {
-    int res;
-    ListNode left;
-
     public int pairSum(ListNode head) {
-        left = head;
-        dfs(head);
-        return res;
-    }
-    private void dfs(ListNode right) {
-        if (right.next != null)
-            dfs(right.next);
-        res = Math.max(res, left.val + right.val);
-        left = left.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+          ListNode pre=null;
+        ListNode curr=slow;
+      
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=pre;
+            pre =curr;
+            curr=next;
+
+        }
+        int max=0;
+        int sum=0;
+        ListNode p1=head;
+        ListNode p2=pre;
+        while(p2!=null){
+            sum=p1.val+p2.val;
+            p1=p1.next;
+            p2=p2.next;
+            if(max<sum){
+                max=sum;
+            }
+        }
+
+     return max;
+
     }
 }
