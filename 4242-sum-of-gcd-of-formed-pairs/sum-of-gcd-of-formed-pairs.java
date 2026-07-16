d208ff0e8244a5981d8c9917eff38be6b24de47c
Khyati Uttam <ukhyati05@gmail.com>
2026-07-16 18:40:30 +0530

    Time: 53 ms (95.50%) | Memory: 107.7 MB (96.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,28 +1,33 @@
-class Solution{
-    public int gcd(int a, int b){
-        if( b == 0 ) return a;
-        return gcd(b, a%b);
-    }
-
-    public long gcdSum( int[] nums ){
+class Solution {
+    public long gcdSum(int[] nums) {
         int n = nums.length;
-        int prefixGcd[] = new int[n];
-        int max = nums[0];
-
-        for ( int i = 0; i < n; i++){
-            max = Math.max(max,nums[i]);
-            prefixGcd[i] = gcd(nums[i],max);
+        int mx = 0;
+        int[] prefixGcd = new int[n];
+        for(int i=0;i<n;i++) {
+            int num = nums[i];
+            mx = Math.max(mx, num);
+            prefixGcd[i] = gcd(mx, num);
         }
-    
-    Arrays.sort(prefixGcd);
 
-    long sum = 0;
-    int i = 0, j = n-1;
-    while (i<j){
-        sum += (long) gcd (prefixGcd[i],prefixGcd[j]);
-        i++;
-        j--;
+        Arrays.sort(prefixGcd);
+
+        int i=0, j=n-1;
+        long sum = 0;
+        while(i < j) {
+            sum += gcd(prefixGcd[i], prefixGcd[j]);
+            i++;
+            j--;
+        }
+
+        return sum;
     }
-    return sum;
-}
+
+    private int gcd(int a, int b) {
+        while(b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
 }
