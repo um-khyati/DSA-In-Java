fabfd45cb041c3fa7bebb9599918970f23bd582a
Khyati Uttam <ukhyati05@gmail.com>
2026-09-16 22:46:51 +0530

    Time: 56 ms (52.90%) | Memory: 93.8 MB (49.68%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,37 +1,27 @@
 class Solution {
-    long[][][] dp;
-    long MOD = 1000000007;
 
-    int solve(int idx, int start, int n, int k) {
-        if (k == 0)
-            return 1;
-
-        if (idx == n)
-            return 0;
-
-        if (dp[idx][k][start] != -1)
-            return (int) dp[idx][k][start];
-
-        long ans = 0;
-
-        if (start == 1) {
-            ans = (ans + solve(idx, 0, n, k - 1)) % MOD;
-            ans = (ans + solve(idx + 1, 1, n, k)) % MOD;
-        } else {
-            ans = (ans + solve(idx + 1, 1, n, k)) % MOD;
-            ans = (ans + solve(idx + 1, 0, n, k)) % MOD;
-        }
-
-        return (int) (dp[idx][k][start] = ans);
-    }
+    private static final int MOD = 1_000_000_007;
 
     public int numberOfSets(int n, int k) {
-        dp = new long[n + 1][k + 1][2];
 
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= k; j++)
-                dp[i][j][0] = dp[i][j][1] = -1;
+        long[][] dp = new long[n][k + 1];
+        long[][] prefix = new long[n][k + 1];
 
-        return solve(0, 0, n, k);
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+            prefix[i][0] = i + 1;
+        }
+
+        for (int j = 1; j <= k; j++) {
+
+            for (int i = 1; i < n; i++) {
+
+                dp[i][j] = (dp[i - 1][j] + prefix[i - 1][j - 1]) % MOD;
+
+                prefix[i][j] = (prefix[i - 1][j] + dp[i][j]) % MOD;
+            }
+        }
+
+        return (int) dp[n - 1][k];
     }
 }
