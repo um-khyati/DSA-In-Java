ecfd29327e32421a4fe63ea909e0bc24b8046c83
Khyati Uttam <ukhyati05@gmail.com>
2026-09-22 13:37:14 +0530

    Time: 205 ms (89.47%) | Memory: 277.4 MB (63.16%) - LeetSync

--- 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.java
+++ 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.java
@@ -1,126 +1,126 @@
-class Solution {
-    int[] tree_prod;
-    int[][] tree_cnt;
-    int K;
-
-    class Node {
-        int prod;
-        int[] cnt;
-        Node() {
-            cnt = new int[K];
-        }
-    }
-
-    public int[] resultArray(int[] nums, int k, int[][] queries) {
+class Solution 
+{
+    public int[] resultArray(int[] nums, int k, int[][] queries) 
+    {
         int n = nums.length;
-        this.K = k;
-        tree_prod = new int[4 * n];
-        tree_cnt = new int[4 * n][k];
-        build(1, 0, n - 1, nums);
+        SegmentTree seg = new SegmentTree(nums, k);
+        int[] ans = new int[queries.length];
 
-        int[] result = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int index = queries[i][0];
-            int value = queries[i][1];
-            int start = queries[i][2];
-            int x = queries[i][3];
+        for (int i = 0; i < queries.length; i++) 
+        {
+            int[] q = queries[i];
+            int index = q[0];
+            int value = q[1];
+            int start = q[2];
+            int x = q[3];
 
+            seg.update(1, 0, n - 1, index, value);
+            int[] pre = seg.query(1, 0, n - 1, start, n - 1);
+            ans[i] = pre[x];
+        }
+
+        return ans;
+    }
+
+    private class SegmentTree 
+    {
+        private static final int MAXK = 6;
+        private int k;
+        private int n;
+        private int[][] tree;
+
+        private SegmentTree(int[] nums, int k) 
+        {
+            this.k = k;
+            this.n = nums.length;
+            int size = 2 << Integer.toBinaryString(n).length();
+            tree = new int[size][MAXK];
+            build(nums, 1, 0, n - 1);
+        }
+
+        private void makeLeaf(int o, int value) 
+        {
+            Arrays.fill(tree[o], 0);
+            int r = value % k;
+            tree[o][r] = 1;
+            tree[o][k] = r;
+        }
+
+        private void mergePre(int[] left, int[] right, int[] result) 
+        {
+            int mulL = left[k];
+            int mulR = right[k];
+            result[k] = (mulL * mulR) % k;
+
+            for (int x = 0; x < k; x++) 
+            {
+                result[x] = left[x];
+            }
+            for (int x = 0; x < k; x++) {
+                result[(mulL * x) % k] += right[x];
+            }
+        }
+
+        private void maintain(int o) 
+        {
+            mergePre(tree[o * 2], tree[o * 2 + 1], tree[o]);
+        }
+
+        private void build(int[] nums, int o, int l, int r) 
+        {
+            if (l == r) 
+            {
+                makeLeaf(o, nums[l]);
+                return;
+            }
+            int m = (l + r) / 2;
+            build(nums, o * 2, l, m);
+            build(nums, o * 2 + 1, m + 1, r);
+            maintain(o);
+        }
+
+        public void update(int o, int l, int r, int index, int value) 
+        {
+            if (l == r) 
+            {
+                makeLeaf(o, value);
+                return;
+            }
+            int m = (l + r) / 2;
             
-            update(1, 0, n - 1, index, value);
-            
-    
-            Node resNode = query(1, 0, n - 1, start, n - 1);
-            result[i] = resNode.cnt[x];
-        }
-        
-        return result;
-    }
-
-    private void build(int node, int l, int r, int[] nums) {
-        if (l == r) {
-            tree_prod[node] = nums[l] % K;
-            tree_cnt[node][nums[l] % K] = 1;
-            return;
-        }
-        int mid = l + (r - l) / 2;
-        build(2 * node, l, mid, nums);
-        build(2 * node + 1, mid + 1, r, nums);
-        pushUp(node);
-    }
-
-    private void pushUp(int node) {
-        int left = 2 * node;
-        int right = 2 * node + 1;
-        
-       
-        tree_prod[node] = (tree_prod[left] * tree_prod[right]) % K;
-        
- 
-        for (int i = 0; i < K; i++) {
-            tree_cnt[node][i] = tree_cnt[left][i];
-        }
-        
-       
-        for (int i = 0; i < K; i++) {
-            if (tree_cnt[right][i] > 0) {
-                int new_val = (tree_prod[left] * i) % K;
-                tree_cnt[node][new_val] += tree_cnt[right][i];
+            if (index <= m) 
+            {
+                update(o * 2, l, m, index, value);
+            } 
+            else 
+            {
+                update(o * 2 + 1, m + 1, r, index, value);
             }
-        }
-    }
-
-    private void update(int node, int l, int r, int idx, int val) {
-        if (l == r) {
-            tree_prod[node] = val % K;
-            for (int i = 0; i < K; i++) {
-                tree_cnt[node][i] = 0;
-            }
-            tree_cnt[node][val % K] = 1;
-            return;
-        }
-        
-        int mid = l + (r - l) / 2;
-        if (idx <= mid) {
-            update(2 * node, l, mid, idx, val);
-        } else {
-            update(2 * node + 1, mid + 1, r, idx, val);
-        }
-        pushUp(node);
-    }
-
-    private Node query(int node, int l, int r, int ql, int qr) {
-        if (ql <= l && r <= qr) {
-            Node res = new Node();
-            res.prod = tree_prod[node];
-            for (int i = 0; i < K; i++) {
-                res.cnt[i] = tree_cnt[node][i];
-            }
-            return res;
-        }
-        
-        int mid = l + (r - l) / 2;
-        if (qr <= mid) {
-            return query(2 * node, l, mid, ql, qr);
-        }
-        if (ql > mid) {
-            return query(2 * node + 1, mid + 1, r, ql, qr);
+            maintain(o);
         }
 
-        
-        Node leftNode = query(2 * node, l, mid, ql, qr);
-        Node rightNode = query(2 * node + 1, mid + 1, r, ql, qr);
-        
-        Node res = new Node();
-        res.prod = (leftNode.prod * rightNode.prod) % K;
-        for (int i = 0; i < K; i++) {
-            res.cnt[i] = leftNode.cnt[i];
-        }
-        for (int i = 0; i < K; i++) {
-            if (rightNode.cnt[i] > 0) {
-                int new_val = (leftNode.prod * i) % K;
-                res.cnt[new_val] += rightNode.cnt[i];
+        public int[] query(int o, int l, int r, int L, int R) 
+        {
+            if (L <= l && r <= R) 
+            {
+                return tree[o];
             }
+
+            int m = (l + r) / 2;
+            if (R <= m) 
+            {
+                return query(o * 2, l, m, L, R);
+            }
+            if (L > m) 
+            {
+                return query(o * 2 + 1, m + 1, r, L, R);
+            }
+
+            int[] left = query(o * 2, l, m, L, R);
+            int[] right = query(o * 2 + 1, m + 1, r, L, R);
+            int[] result = new int[MAXK];
+            mergePre(left, right, result);
+            return result;
         }
-        return res;
     }
 }
