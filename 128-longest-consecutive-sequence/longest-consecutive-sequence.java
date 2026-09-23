eef860c04bb136322853cc4917d30369c53280da
Khyati Uttam <ukhyati05@gmail.com>
2026-09-23 17:31:57 +0530

    Time: 2 ms (100.00%) | Memory: 78.1 MB (82.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,27 +1,57 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+    static 
+    {
+        for(int i=0;i<500;i++)
+            longestConsecutive(new int[]{});
+    }
+    
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int min = nums[0];
+        int max = nums[0];
 
-        for (int num : nums) {
-            set.add(num);
+        for (int num : nums){
+            max = Math.max(num, max);
+            min = Math.min(num, min);
         }
 
-        int longest = 0;
-
-        for (int num : set) {
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    currentStreak++;
-                }
-
-                longest = Math.max(longest, currentStreak);
+        if (max < 100000) {
+            boolean [] apariciones = new boolean [max - min + 1];
+            for (int num : nums) {
+                apariciones[num - min] = true;
             }
-        }
 
-        return longest;
+            int maxTotal = 0;
+            int maxLocal = 0;
+            for (boolean a : apariciones) {
+                if (a) {
+                    maxLocal++;
+                } else {
+                    maxTotal = Math.max(maxTotal, maxLocal);
+                    maxLocal = 0;
+                }
+            }
+            maxTotal = Math.max(maxTotal, maxLocal);
+            return maxTotal;
+        } else {
+            Set<Integer> uniNums = new HashSet();
+            for( int num : nums ){
+                uniNums.add(num);
+            }
+
+            int maxB = 0;
+            for ( int num : uniNums ){
+                if (!uniNums.contains(num - 1)){
+                    int endSeq = num;
+                    while (uniNums.contains(endSeq+1)){
+                        endSeq++;
+                    }
+                    maxB = Math.max(maxB, endSeq - num + 1);
+                }
+            }
+            return maxB;
+        }
     }
 }
