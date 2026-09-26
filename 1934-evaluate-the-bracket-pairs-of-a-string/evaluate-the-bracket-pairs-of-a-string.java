1161c22ec1f48bbbe0e30e883af4f20641b4dec5
Khyati Uttam <ukhyati05@gmail.com>
2026-09-26 13:25:23 +0530

    Time: 28 ms (100.00%) | Memory: 97.6 MB (20.36%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,30 +1,24 @@
-
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> knowledgeMap = new HashMap<>(knowledge.size());
+
         for (List<String> pair : knowledge) {
-            map.put(pair.get(0), pair.get(1));
+            knowledgeMap.put(pair.get(0), pair.get(1));
         }
-        
+
         StringBuilder result = new StringBuilder();
-        StringBuilder keyBuilder = new StringBuilder();
-        boolean insideBrackets = false;
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                insideBrackets = true;
-                keyBuilder = new StringBuilder();
-            } else if (c == ')') {
-                insideBrackets = false;
-                String key = keyBuilder.toString();
-                result.append(map.getOrDefault(key, "?"));
-            } else if (insideBrackets) {
-                keyBuilder.append(c);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                int closingBracketIndex = s.indexOf(')', i + 1);
+                String key = s.substring(i + 1, closingBracketIndex);
+                result.append(knowledgeMap.getOrDefault(key, "?"));
+                i = closingBracketIndex;
             } else {
-                result.append(c);
+                result.append(s.charAt(i));
             }
         }
-        
+
         return result.toString();
     }
 }
